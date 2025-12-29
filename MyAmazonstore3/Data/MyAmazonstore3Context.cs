5cbf5e169e19dffec8bcc1eed010c0c43c0cd17e
soussichahd <soussichahd3112@gmail.com>
2025-12-29 15:51:08 +0100

    Message décrivant mes modifications

--- MyAmazonstore3/Data/MyAmazonstore3Context.cs
+++ MyAmazonstore3/Data/MyAmazonstore3Context.cs
@@ -16,11 +16,8 @@ namespace MyAmazonstore3.Data
 
         public DbSet<MyAmazonstore3.Models.Produit> Produit { get; set; } = default!;
         public DbSet<MyAmazonstore3.Models.Categorie> Categorie { get; set; } = default!;
-<<<<<<< HEAD
         
-=======
-       
->>>>>>> 1c2ddfc245832854bdad923b7887e7d48c0085e7
         public DbSet<MyAmazonstore3.Models.User> User { get; set; } = default!;
     }
 }
+
